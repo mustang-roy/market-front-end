7115daae77333b20433f80d7f1bb07564ca50dea
Daniel Carvalho <daanwhat@gmail.com>
2020-10-28 15:22:09 -0300

    Agora vai Jesus

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Mainpage from "./pages/Mainpage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchData: 'Faz ae Luiz',
+      searchText: ''
+    };
+
+    this.onSearchData = this.onSearchData.bind(this)
+  }
+
+  onSearchData(event) {
+    this.setState({
+      searchText: event.target.value 
+    })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Mainpage
+                searchText={this.state.searchText}
+                searchData={this.state.searchData}
+                onSearchData={this.onSearchData}
+              />
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Cart.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class CartIcon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opened: false,
+    };
+
+    this.openCart = this.openCart.bind(this);
+  }
+
+  openCart() {
+    this.setState({
+      opened: !this.state.opened,
+    });
+  }
+
+  render() {
+
+    const {results} = this.props.data;
+    console.log(this.props.data)
+
+
+    return (
+      
+      <div>
+        <button onClick={this.openCart}>
+          <h1>icon</h1>
+        </button>
+        {this.state.opened && (
+          <div>
+            <button onClick={this.openCart}>x</button>
+            <h1>Carrinho de Compras</h1>
+            {results.map((item) => {
+              return (
+                <div key={item.id}>
+                  <h1>{item.title}</h1>
+                  <p>Quantidade: {Math.floor(Math.random()*10)}</p>
+                  <p>R${item.price}</p>
+                </div>
+              );
+            })}
+            <Link to="/">Ir para o Carrinho</Link>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default CartIcon;
