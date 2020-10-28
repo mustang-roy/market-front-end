1de9ed400e550267d11918134afc00654ce63ae2
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-28 16:08:01 -0300

    code clean on App and rename main page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Mainpage from "./pages/Mainpage";
+
+import MainPage from "./pages/MainPage";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      searchData: 'Faz ae Luiz',
-      searchText: ''
-    };
 
-    this.onSearchData = this.onSearchData.bind(this)
-  }
+    this.onSearchText = this.onSearchText.bind(this);
 
-  onSearchData(event) {
-    this.setState({
-      searchText: event.target.value 
-    })
-  }
+    this.state = { searchText: '' };
+  };
+
+  onSearchText(event) {
+    const { value } = event.target;
+    this.setState({ searchText: value });
+  };
 
   render() {
+    const { searchText } = this.state;
+
     return (
       <BrowserRouter>
         <Switch>
           <Route
             exact
             path="/"
-            render={() => (
-              <Mainpage
-                searchText={this.state.searchText}
-                searchData={this.state.searchData}
-                onSearchData={this.onSearchData}
-              />
-            )}
+            render={() => <MainPage searchText={searchText} onSearchText={this.onSearchText} />}
           />
         </Switch>
       </BrowserRouter>
     );
-  }
+  };
 }
 
 export default App;
