5495407eb8535aac05e0f4000a0f605a8f802386
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-10-29 10:14:39 -0300

    ShoppingDetails renders an item

--- src/components/ShoppingDetails/index.js
+++ src/components/ShoppingDetails/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function ShoppingDetails() {
-  return <p>ShoppingDetails</p>;
+import { Wrapper } from './style';
+
+function ShoppingDetails({ item }) {
+  const { title, price } = item;
+
+  const [quantity, setQuantity] = useState(item.quantity);
+  const [total, setTotal] = useState(price * quantity);
+
+  const { pathname: page } = window.location;
+
+  const increaseQuantity = () => setQuantity(quantity + 1);
+  const decreaseQuantity = () => quantity > 0 && setQuantity(quantity - 1);
+
+  const increaseButton = <button onClick={increaseQuantity}>+</button>;
+  const decreaseButton = <button onClick={decreaseQuantity}>-</button>;
+
+  useEffect(() => {
+    setTotal(price * quantity)
+  }, [price, quantity]);
+
+  return (
+    <Wrapper>
+      <div>{title}</div>
+
+      {page === 'cart' && increaseButton}
+
+      <div>{quantity}</div>
+
+      {page === 'cart' && decreaseButton}
+
+      <div>R$ {price}</div>
+      <div>R$ {total}</div>
+    </Wrapper>
+  );
 }
+
+ShoppingDetails.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default ShoppingDetails;
--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -5,11 +5,17 @@ import data from '../../__mocks__/detailPage';
 import DefaultWrapper from '../../components/DefaultWrapper';
 import ShoppingDetails from '../../components/ShoppingDetails';
 
+const item = {
+  title: 'Samsung Odyssey',
+  quantity: 3,
+  price: 2500,
+};
+
 function DetailPage() {
   const { detailPage } = data;
 
   return (
-    <DefaultWrapper wrapperInfo={detailPage} render={<ShoppingDetails />}>
+    <DefaultWrapper wrapperInfo={detailPage} render={<ShoppingDetails item={item}/>}>
       <h1>Content</h1>
     </DefaultWrapper>
   );
