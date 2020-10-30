e2a0af29132ae948980a147cb9acf15ab41b0859
Hercules Gabriel <51159478+herculesgabriel@users.noreply.github.com>
2020-10-30 18:58:37 -0300

    Update src/services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,6 @@ export async function getProductsFromCategoryAndQuery( categoryId = '', query = 
   const category = categoryId === '' ? '': `category=$${categoryId}`;
   const id = query === '' ? '': `query=$${categoryId}`;
   const joinElement = categoryId === '' ? '': '&';
-
   
   const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?${category}${joinElement}${id}`);
   const resultJson = await result.json();
