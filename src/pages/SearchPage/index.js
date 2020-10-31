93e20cd3fc8a380ce4ea9c634b36cae53b9e93df
Luiz F S Mariz <lfsmariz@gmail.com>
2020-10-30 23:37:03 -0300

    update search folder

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import SearchBar from "../components/SearchBar";
-import Categorias from "../components/Categorias";
-import Button from '../components/Button';
-import * as API from "../services/api"
+import SearchBar from "../../components/SearchBar";
+import Categorias from "../../components/Categorias";
+import Button from "../../components/Button";
+import * as API from "../../services/api";
 // import categoriasData from "../__mocks__/categories"
 // // import OrderItems from "../components/OrderItems";
 // // import Cart from "../components/Cart";
 // // import data from "../__mocks__/query";
-import Item from "../components/Item";
+import Item from "../../components/Item";
 // // import dataSearch from "../__mocks__/query"
 
 class SearchPage extends Component {
@@ -17,9 +17,9 @@ class SearchPage extends Component {
     super(props);
     this.state = {
       listCategories: [],
-      searchText: '',
-      dataSearch: []
-    }
+      searchText: "",
+      dataSearch: [],
+    };
 
     this.onSearchText = this.onSearchText.bind(this);
     this.onClickPesquisar = this.onClickPesquisar.bind(this);
@@ -43,34 +43,47 @@ class SearchPage extends Component {
   onSearchText(event) {
     const { value } = event.target;
     this.setState({ searchText: value });
-  };
+  }
 
   onClickPesquisar() {
-    API.getProductsFromCategoryAndQuery('', this.state.searchText).
-    then((result) => this.setState({dataSearch: result}))
+    API.getProductsFromCategoryAndQuery(
+      "",
+      this.state.searchText
+    ).then((result) => this.setState({ dataSearch: result }));
   }
 
   componentDidMount() {
-    API.getCategories().
-      then((result) => this.setState({ listCategories: result }))
+    API.getCategories().then((result) =>
+      this.setState({ listCategories: result })
+    );
   }
 
   render() {
     return (
       <div>
-        <h1 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma
-      categoria.</h1>
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
         <Categorias data={this.state.listCategories} />
         <SearchBar
           searchText={this.state.searchText}
           onSearchText={this.onSearchText}
         />
-        <button data-testid="query-button" onClick={this.onClickPesquisar}>Pesquisar</button>
+        <button data-testid="query-button" onClick={this.onClickPesquisar}>
+          Pesquisar
+        </button>
         {/* <OrderItems onSortToggle={this.onSortToggle} value={this.state.sort}/>
         <Cart data={data} /> */}
         <div>
-          {this.state.dataSearch.length != 0 && this.state.dataSearch.results.map(item => <Item data={item} key={item.id} onClickComprar={this.onClickComprar}/>)}
-        </div> */}
+          {this.state.dataSearch.length !== 0 &&
+            this.state.dataSearch.results.map((item) => (
+              <Item
+                data={item}
+                key={item.id}
+                onClickComprar={this.onClickComprar}
+              />
+            ))}
+        </div>
         <Button as={Link} to="/cart" data-testid="shopping-cart-button">
           Cart
         </Button>
@@ -78,6 +91,5 @@ class SearchPage extends Component {
     );
   }
 }
-
 
 export default SearchPage;
